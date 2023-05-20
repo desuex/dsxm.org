4b1ffc9b3e78d2809be3a3f79e2efb4ae2dd05b1
desuex <me@dsxm.org>
2023-05-21 00:03:32 +0600

    simple api client for di.dm

---
+++ radio/app/Services/AudioAddictService.php
@@ -0,0 +1,63 @@
+<?php
+namespace App\Services;
+use GuzzleHttp\Client;
+
+class AudioAddictService 
+{
+    protected $httpClient;
+
+    public function __construct(Client $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    public function getCurrentlyPlaying(string $service = 'di'): array
+    {
+        // Make a GET request to the API endpoint
+        $response = $this->httpClient->get("https://api.audioaddict.com/v1/{$service}/currently_playing");
+
+        // Get the response body as JSON
+        $tracks = json_decode($response->getBody(), true);
+
+        // Return the discovered tracks
+        return $tracks;
+    }
+
+    public function getChannels(string $service = 'di'): array
+    {
+        // Make a GET request to the API endpoint
+        $response = $this->httpClient->get("https://api.audioaddict.com/v1/{$service}/channels");
+
+        // Get the response body as JSON
+        $channels = json_decode($response->getBody(), true);
+
+        // Return the discovered tracks
+        return $channels;
+    }
+
+    public function getTrackDetails(int $trackId, string $service = 'di'): array
+    {
+        // Make a GET request to the API endpoint
+        $response = $this->httpClient->get("https://api.audioaddict.com/v1/{$service}/tracks/{$trackId}");
+
+        // Get the response body as JSON
+        $trackDetails = json_decode($response->getBody(), true);
+
+        // Return the track details
+        return $trackDetails;
+    }
+
+    public function getArtistDetails(int $artistId, string $service = 'di'): array
+    {
+        // Make a GET request to the API endpoint
+        $response = $this->httpClient->get("https://api.audioaddict.com/v1/{$service}/artists/{$artistId}");
+
+        // Get the response body as JSON
+        $artistDetails = json_decode($response->getBody(), true);
+
+        // Return the track details
+        return $artistDetails;
+    }
+
+
+}
